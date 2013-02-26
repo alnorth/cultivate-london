aef5a4e1fc75ae379a4a38661d821b3ebbb81fe7
Alasdair North <alnorth@gmail.com>
2013-02-26 09:05:12 +0000

    Rename application to Cultivate London.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Vagrant::Application.config.secret_token = '7ae2db23f14882d83608f23f4d18f90dc5d50c4f2e507a85cea52ba8d6b335ee085806a2c54e61b1c788e6ccbc31ba4cddfab5a6984a72e83c2569c54db1eb65'
+CultivateLondon::Application.config.secret_token = '7ae2db23f14882d83608f23f4d18f90dc5d50c4f2e507a85cea52ba8d6b335ee085806a2c54e61b1c788e6ccbc31ba4cddfab5a6984a72e83c2569c54db1eb65'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Vagrant::Application.routes.draw do
+CultivateLondon::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
