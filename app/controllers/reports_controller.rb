3277dacdc164bbf9b16fcc501bef75728ad77049
Alasdair North <alnorth@gmail.com>
2013-03-14 11:03:16 +0000

    Showing batches in a report.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,7 @@
 class ReportsController < ApplicationController
   def show
-
+    d = Date.today
+    @week_number = d.cweek
+    @batches = Batch.includes([:site, :category, :crop, :size])
   end
 end
