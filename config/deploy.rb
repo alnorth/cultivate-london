4b75994c9b71a53681166bce02832ff1379fb140
Alasdair North <alnorth@gmail.com>
2013-03-14 12:57:00 +0000

    Add task for loading schema

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,3 +15,11 @@ set :use_sudo, false
 set :stages, ["staging", "production"]
 set :default_stage, "staging"
 require 'capistrano/ext/multistage'
+
+namespace :deploy do
+  desc 'Load DB schema - CAUTION: rewrites database!'
+  task :load_schema, :roles => :app do
+    raise RuntimeError.new('load_schema aborted!') unless Capistrano::CLI.ui.ask("Are you sure to wipe the entire database (anything other than 'yes' aborts):") == 'yes'
+    run "cd #{current_path}; bundle exec rake db:schema:load RAILS_ENV=#{rails_env}"
+  end
+end
