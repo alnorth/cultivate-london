44ae599c95ff4cb3b9bbedd93156c2a2a8e8d25f
Alasdair North <alnorth@gmail.com>
2013-09-25 17:25:48 +0100

    Fix for routes like /users/sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 CultivateLondon::Application.routes.draw do
-  resources :users, :except => [:show]
+  resources :users, :constraints => {:id => /[0-9]+/}, :except => [:show]
   devise_for :users, :skip => [:registrations]
   as :user do
     get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
