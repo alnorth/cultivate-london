b9ff5a91b317382a725b3d9ab2b3d13208247d0d
Alasdair North <alnorth@gmail.com>
2013-09-25 17:21:36 +0100

    Use load_and_authorize_resource to secure users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,20 @@
 class UsersController < ApplicationController
+  load_and_authorize_resource
+
   # GET /users
   # GET /users.json
   def index
-    @users = User.order('email').all
-
+    @users = @users.order(:email)
+    
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @users }
     end
   end
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user }
-    end
-  end
-
   # GET /users/new
   # GET /users/new.json
   def new
-    @user = User.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @user }
@@ -34,14 +23,11 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
-    @user = User.find(params[:id])
   end
 
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(params[:user])
-
     respond_to do |format|
       if @user.save
         format.html { redirect_to users_path, notice: 'User was successfully created.' }
@@ -56,8 +42,6 @@ class UsersController < ApplicationController
   # PUT /users/1
   # PUT /users/1.json
   def update
-    @user = User.find(params[:id])
-
     respond_to do |format|
       if @user.update_attributes(params[:user])
         format.html { redirect_to users_path, notice: 'User was successfully updated.' }
@@ -72,7 +56,6 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
 
     respond_to do |format|
